da234578ad1ad569ce852080a458735b784a9931
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-06-16 00:37:35 +0900

    [setting] 프로젝트 초기 세팅

---
+++ src/main/java/inha/cumulonimbus_cloud/CumulonimbusCloudApplication.java
@@ -0,0 +1,13 @@
+package inha.cumulonimbus_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CumulonimbusCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CumulonimbusCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inha/cumulonimbus_cloud/CumulonimbusCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package inha.cumulonimbus_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CumulonimbusCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
