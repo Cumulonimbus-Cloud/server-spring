743922101bebcd833b4702f1df2518f5df2a1ffa
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-06-17 17:14:52 +0900

    [feat/#24] 크롤링 개발 완료 (#27)
    * [refactor/#21] 챗봇 대화 및 조회 수정 (#23)
    
    * [setting] 이슈 템플릿 추가
    
    * [setting] deploy 구문 수정
    
    * [setting] application.yml 설정 추가
    
    * [setting] application.yml 설정 추가
    
    * [setting] application.yml 설정 추가
    
    * [setting/#1] 프로필 분리 및 스웨거 설정 추가
    
    * [setting/#1] appspec-dev.yml, appspec-dev.prod로 분리
    
    * [setting/#1] appspec-dev.yml, appspec-dev.prod로 분리
    
    * [setting/#1] appspec-dev.yml, appspec-dev.prod로 분리
    
    * [setting/#1] appspec-dev.yml, appspec-dev.prod로 분리
    
    * [setting/#1] api 응답 및 에러 통일
    
    * [setting/#1] 환경변수 추가
    
    * [feat/#9] 카카오 로그인 구현
    
    * [feat/#9] 카카오 로그인 구현
    
    * [feat/#9] 카카오 로그인 구현
    
    * [feat/#9] 카카오 로그인 구현
    
    * [refactor/#10]  배포 스크립트 수정
    
    * [refactor/#10]  배포 스크립트 수정
    
    * [refactor/#10]  배포 스크립트 수정
    
    * [refactor/#10]  배포 스크립트 수정
    
    * [refactor/#10]  배포 스크립트 수정
    
    * [refactor/#18] 플라스크 서버 통신을 통한 배드락 호출
    
    * [feat/#18] 플라스크 서버 통신을 통한 챗봇 사용 및 조회 기능 추가
    
    * [feat/#24] s3 저장 기능 추가
    
    * [feat/#24] 크롤링 개발 완료

--- src/main/java/inha/cumulonimbus_cloud/grade_card/controller/GradeCardController.java
+++ src/main/java/inha/cumulonimbus_cloud/grade_card/controller/GradeCardController.java
@@ -1,12 +1,19 @@
 package inha.cumulonimbus_cloud.grade_card.controller;
 
+import inha.cumulonimbus_cloud.common.BaseResponse;
+import inha.cumulonimbus_cloud.grade_card.controller.dto.response.PostGradeCardRes;
 import inha.cumulonimbus_cloud.grade_card.service.GradeCardService;
+import inha.cumulonimbus_cloud.user.domain.User;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import static inha.cumulonimbus_cloud.common.code.status.SuccessStatus.POST_GRADE_OK;
 
 @Slf4j
 @Tag(name = "grade_card controller", description = "성적표 관련 API")
@@ -17,5 +24,11 @@ public class GradeCardController {
 
     private final GradeCardService gradeCardService;
 
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 
+    @Operation(summary = "성적표 크롤링 API", description = "성적표를 크롤링하여 csv파일로 저장합니다.")
+    public BaseResponse<PostGradeCardRes> uploadGradeCard(@AuthenticationPrincipal User user,
+                                                          @RequestParam("file") MultipartFile file) {
+        return BaseResponse.of(POST_GRADE_OK, gradeCardService.uploadGradeCard(file, user.getId()));
+    }
 }
