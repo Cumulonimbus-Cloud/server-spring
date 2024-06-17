6fa43bec3737d429e9e482f42f7bd0545ef7e4d9
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-06-17 21:28:08 +0900

    [refactor/#33] cors 수정 (#40)
    * [feat/#33] 성적표 수정 api 추가
    
    * [feat/#33] 모든 클라이언트 사이드 경로 요청 index.html로 변경
    
    * [feat/#33] 모든 클라이언트 사이드 경로 요청 index.html로 변경
    
    * [refactor/#33] cors 수정

--- src/main/java/inha/cumulonimbus_cloud/utils/RoutingFilter.java
+++ src/main/java/inha/cumulonimbus_cloud/utils/RoutingFilter.java
@@ -3,7 +3,6 @@ package inha.cumulonimbus_cloud.utils;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -11,24 +10,27 @@ import java.util.Arrays;
 import java.util.List;
 
 @Component
-@Slf4j
 public class RoutingFilter implements Filter {
+
     private static final List<String> staticResources = Arrays.asList(
-            "/css", "/js", "/img", "/fonts", "/favicon.ico", "/manifest.json", "/static/"
+            "/css", "/js", "/img", "/fonts", "/favicon.ico", "/manifest.json", "/static/", "/swagger-ui", "/v3"
     );
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
 
-        log.info("Request URI: {}", req.getRequestURI());
         String path = req.getRequestURI().substring(req.getContextPath().length());
-        if(staticResources.stream().noneMatch(path::startsWith)){
-            if(!path.startsWith("/api/v1") && !path.startsWith("/swagger-ui") && !path.startsWith("/v3")) {
+
+        // 정적 리소스나 스웨거 UI 경로를 제외한 경로에 대해 index.html로 포워딩
+        if (staticResources.stream().noneMatch(path::startsWith)) {
+            if (!path.startsWith("/api/v1")) {
                 req.getRequestDispatcher("/index.html").forward(req, res);
                 return;
             }
         }
+
         filterChain.doFilter(request, response);
     }
 }
