615bf605c528f1c70c101704cd275b43b54cd29f
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-06-17 13:33:29 +0900

    [feat/#18] 플라스크 서버 통신을 통한 챗봇 사용 및 조회 기능 추가 (#20)
    * [setting] 이슈 템플릿 추가
    
    * [setting] deploy 구문 수정
    
    * [setting] application.yml 설정 추가
    
    * [setting] application.yml 설정 추가
    
    * [setting] application.yml 설정 추가
    
    * [setting/#1] 프로필 분리 및 스웨거 설정 추가
    
    * [setting/#1] appspec-dev.yml, appspec-dev.prod로 분리
    
    * [setting/#1] appspec-dev.yml, appspec-dev.prod로 분리
    
    * [setting/#1] appspec-dev.yml, appspec-dev.prod로 분리
    
    * [setting/#1] appspec-dev.yml, appspec-dev.prod로 분리
    
    * [setting/#1] api 응답 및 에러 통일
    
    * [setting/#1] 환경변수 추가
    
    * [feat/#9] 카카오 로그인 구현
    
    * [feat/#9] 카카오 로그인 구현
    
    * [feat/#9] 카카오 로그인 구현
    
    * [feat/#9] 카카오 로그인 구현
    
    * [refactor/#10]  배포 스크립트 수정
    
    * [refactor/#10]  배포 스크립트 수정
    
    * [refactor/#10]  배포 스크립트 수정
    
    * [refactor/#10]  배포 스크립트 수정
    
    * [refactor/#10]  배포 스크립트 수정
    
    * [refactor/#18] 플라스크 서버 통신을 통한 배드락 호출
    
    * [feat/#18] 플라스크 서버 통신을 통한 챗봇 사용 및 조회 기능 추가

--- src/main/java/inha/cumulonimbus_cloud/common/config/SecurityConfig.java
+++ src/main/java/inha/cumulonimbus_cloud/common/config/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
     private static final String[] WHITE_LIST_URL = {
             "/",
             "/api/v1/auth/**",
+            "/api/v1/chat/**",
             "/api/v1/test/**",
             "/v2/api-docs",
             "/v3/api-docs",
